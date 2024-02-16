b5e73727fd8276ab22510cfb803ada90812db8af
Justin Lopato <justin@lopato.org>
2024-02-15 20:31:51 -0600

    Delete ros2_ws/build directory

--- ros2_ws/build/simple_node/build/lib/simple_node/simple_node.py
+++
@@ -1,9 +0,0 @@
-import rclpy
-from rclpy.node import Node
-def main(args=None):
-    rclpy.init(args=args)
-    node = Node('my_node_name')
-    rclpy.spin(node)
-    rclpy.shutdown()
-if __name__ == '__main__':
-    main()
--- ros2_ws/build/simple_node/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/lopatoj/Projects/raspi4/ros2_ws/install/simple_node'
