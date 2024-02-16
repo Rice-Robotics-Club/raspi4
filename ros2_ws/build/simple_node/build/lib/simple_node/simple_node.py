3706537bce6f9aad0d60691aa38a4d3c551af77a
Justin Lopato <justin@lopato.org>
2024-02-15 20:18:33 -0600

    created .gitignore

--- ros2_ws/build/simple_node/build/lib/simple_node/simple_node.py
+++ ros2_ws/build/simple_node/build/lib/simple_node/simple_node.py
@@ -7,4 +7,3 @@ def main(args=None):
     rclpy.shutdown()
 if __name__ == '__main__':
     main()
-    
--- ros2_ws/src/simple_node/simple_node/simple_node.py
+++ ros2_ws/src/simple_node/simple_node/simple_node.py
@@ -1,5 +1,6 @@
 import rclpy
 from rclpy.node import Node
+
 def main(args=None):
     rclpy.init(args=args)
     node = Node('my_node_name')
