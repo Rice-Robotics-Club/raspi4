e556a7289f848628c62a85a695477d32bcf2c9ca
Justin Lopato <justin@lopato.org>
2024-02-29 20:29:45 -0600

    updated simple node with service and topic for controlling odrive

--- ros2_ws/src/simple_node/simple_node/simple_node.py
+++ ros2_ws/src/simple_node/simple_node/simple_node.py
@@ -1,16 +1,42 @@
 import rclpy
 from rclpy.node import Node
-from odrive_can.msgs import ControllerStatus
-from odrive.enums import ProcedureResult
+from odrive_can.srv import AxisState
+from odrive_can.msg import ControlMessage
+    
 
 class SimpleNode(Node):
     def __init__(self):
         super().__init__('simple_node')
-        self.get_logger().info('Simple Node Started')
+        
+        # Create a client for the set_axis_state service
+        self.state = self.create_client(AxisState, 'request_axis_state')
+        self.axis_request = AxisState.Request()
+        
+        # Create a publisher for the 
+        self.publisher_ = self.create_publisher(ControlMessage, 'control_message', 10)
+        self.timer = self.create_timer(0.1, self.control)
+        
+    def control(self):
+        msg = ControlMessage()
+        msg.control_mode = 2
+        msg.input_mode = 1
+        msg.input_pos = 0.0
+        msg.input_vel = 1.0
+        msg.input_torque = 0.0
+        self.publisher_.publish(msg)
+    
+    def set_axis_state(self, s):
+        self.axis_request.axis_requested_state = s
+        self.future = self.cli.call_async(self.axis_request)
+        rclpy.spin_until_future_complete(self, self.future)
+        self.get_logger().info('Result: %r' % (self.future.result().axis_state))
+        return self.future.result()
+        
 
 def main(args=None):
     rclpy.init(args=args)
     node = SimpleNode()
+    node.set_axis_state(8) # CLOSED_LOOP_CONTROL
     rclpy.spin(node)
     rclpy.shutdown()
     
