51514b7934bb789cb47445a39274b6872804c06e
winsonlin8 <112127031+winsonlin8@users.noreply.github.com>
2024-03-21 19:23:30 -0500

    Update servo_control_node.py
    gpio pin 0

--- ros2_ws/src/simple_node/simple_node/servo_control_node.py
+++ ros2_ws/src/simple_node/simple_node/servo_control_node.py
@@ -1,17 +1,23 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float64
+from gpiozero import Servo
+from time import sleep
 
 class ServoControlNode(Node):
     def __init__(self):
         super().__init__('servo_control_node')
         self.subscription = self.create_subscription(
             Float64, 'servo_angle', self.angle_callback, 10)
+        self.servo = Servo(0)  # Assuming the servo is connected to GPIO pin 0
 
     def angle_callback(self, msg):
         angle = msg.data
         self.get_logger().info(f'Received angle: {angle}')
-        # Controls the servo based on the received angle
+        # gpiozero needs a value between -1 and 1
+        servo_value = angle/90
+        self.servo.value = servo_value
+        sleep(1)
 
 def main(args=None):
     rclpy.init(args=args)
