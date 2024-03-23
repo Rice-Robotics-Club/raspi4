44c8ea8b59a01f86b25f26f4ca91e63f3d3d314d
Justin Lopato <justin@lopato.org>
2024-03-22 20:18:19 -0500

    yay

--- ros2_ws/src/catbot/catbot/stop.py
+++ ros2_ws/src/catbot/catbot/stop.py
@@ -7,12 +7,11 @@ class Stop(Node):
         super().__init__('stop')
         
         # Create clients for the request_axis_state services
-        self.axis_state = []
-        self.axis_state[0] = self.create_client(AxisState, '/odrive_axis0/request_axis_state')
-        self.axis_state[1] = self.create_client(AxisState, '/odrive_axis1/request_axis_state')
+        self.axis_state0 = self.create_client(AxisState, '/odrive_axis0/request_axis_state')
+        self.axis_state1 = self.create_client(AxisState, '/odrive_axis1/request_axis_state')
         
         # Wait for the services to be available
-        while not self.axis_state_0.wait_for_service(timeout_sec=1.0) or not self.axis_state_1.wait_for_service(timeout_sec=1.0):
+        while not self.axis_state0.wait_for_service(timeout_sec=1.0) or not self.axis_state1.wait_for_service(timeout_sec=1.0):
             self.get_logger().info('waiting for AxisState services...')
             
         # Create a request for the request_axis_state services
@@ -20,12 +19,12 @@ class Stop(Node):
         self.axis_request.axis_requested_state = 1 # AXIS_STATE_IDLE
         
         # /odrive_axis0/
-        self.future = self.axis_state[0].call_async(self.axis_request)
+        self.future = self.axis_state0.call_async(self.axis_request)
         rclpy.spin_until_future_complete(self, self.future)
         self.get_logger().info('Stop result: %r' % (self.future.result().axis_state))
         
         # /odrive_axis1/
-        self.future = self.axis_state[1].call_async(self.axis_request)
+        self.future = self.axis_state1.call_async(self.axis_request)
         rclpy.spin_until_future_complete(self, self.future)
         self.get_logger().info('Stop result: %r' % (self.future.result().axis_state))
         
