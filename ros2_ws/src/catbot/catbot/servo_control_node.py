ad13896e5218210dbeeec001e308bd3e03febed1
nicholasl23638 <nicholasl23638@gmail.com>
2024-03-27 17:23:49 -0500

    trying useing gpiozero

--- ros2_ws/src/catbot/catbot/servo_control_node.py
+++ ros2_ws/src/catbot/catbot/servo_control_node.py
@@ -1,22 +1,26 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float64
-from .root_servo_control import ServoWrapper
+from gpiozero import Servo
+#from .root_servo_control import ServoWrapper
 from time import sleep
 
 class ServoControlNode(Node):
-    def __init__(self, servo_wrapper: ServoWrapper):
+    def __init__(self):
         super().__init__('servo_control_node')
-        self.servo = servo_wrapper
+        self.servo = Servo(11)
         self.subscription = self.create_subscription(
             Float64, 'servo_angle', self.angle_callback, 10)
         
-        # just to see if it works :)
         self.get_logger().info("testing time!")
-        try:
-            self.servo.test(2)
-        except:
-            self.get_logger().info("test failed!")
+        self.servo.value = 0 # test is here!
+        
+        # # just to see if it works :)
+        # try:
+        #     self.servo.test(2)
+        # except:
+        #     self.get_logger().info("test failed!")
+
 
     # not using this rn - just testing in init TODO make angle setting work
     def angle_callback(self, msg):
@@ -29,13 +33,13 @@ class ServoControlNode(Node):
 
 def main(args=None):
     rclpy.init(args=args)
-    servo = ServoWrapper(11) # TODO make node init with a non-predetermined id
-    node = ServoControlNode(servo)
+    # servo = ServoWrapper(11) # TODO make node init with a non-predetermined id
+    node = ServoControlNode()
 
     rclpy.spin(node)
 
     node.destroy_node()
-    servo.shutdown()
+    # servo.shutdown()
     rclpy.shutdown()
 
 if __name__ == '__main__':
