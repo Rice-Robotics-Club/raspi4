9c5033eadb1c214a83c8c93d2592dc049eeca289
winsonlin8 <112127031+winsonlin8@users.noreply.github.com>
2024-03-27 19:31:02 -0500

    Update jump_phase_one.py to log messages less frequently

--- ros2_ws/src/catbot/catbot/jump_phase_one.py
+++ ros2_ws/src/catbot/catbot/jump_phase_one.py
@@ -6,7 +6,7 @@ from odrive_can.msg import ControlMessage, ControllerStatus
 class ControlNode(Node):
     def __init__(self):
         super().__init__('simple_node')
-        
+
         # Create a client for the set_axis_state service
         self.state = self.create_client(AxisState, 'request_axis_state')
         while not self.state.wait_for_service(timeout_sec=1.0):
@@ -28,11 +28,9 @@ class ControlNode(Node):
         )
 
         # Create ControlMessage objects for both motors
-        # Top motor
         self.msg0 = ControlMessage()
         self.msg0.control_mode = 2
         self.msg0.input_mode = 1
-        # Bottom Motor
         self.msg1 = ControlMessage()
         self.msg1.control_mode = 2
         self.msg1.input_mode = 1
@@ -42,36 +40,39 @@ class ControlNode(Node):
         self.zeroangle1 = None
 
         # Set up publishers to the ODrives
-        self.control0 = self.create_publisher(ControlMessage, 'control_message_0', 10)  # Top Motor
-        self.control1 = self.create_publisher(ControlMessage, 'control_message_1', 10)  # Bottom Motor
+        self.control0 = self.create_publisher(ControlMessage, 'control_message_0', 10)
+        self.control1 = self.create_publisher(ControlMessage, 'control_message_1', 10)
 
         # Create timer
         self.timer = self.create_timer(0.1, self.control_message)
-    
+
+        # Logging frequency control
+        self.log_counter_axis0 = 0
+        self.log_counter_axis1 = 0
+        self.log_frequency = 25  # Adjust this value to change the logging frequency
+
     def controller_status_callback_axis0(self, msg):
         if self.zeroangle0 is None:
             self.zeroangle0 = msg.pos_estimate
-        self.get_logger().info(f'Axis 0: pos_estimate={msg.pos_estimate}, vel_estimate={msg.vel_estimate}, torque_estimate={msg.torque_estimate}')
+        self.log_counter_axis0 += 1
+        if self.log_counter_axis0 % self.log_frequency == 0:
+            self.get_logger().info(f'Axis 0: pos_estimate={msg.pos_estimate}, vel_estimate={msg.vel_estimate}, torque_estimate={msg.torque_estimate}')
+            self.log_counter_axis0 = 0
 
     def controller_status_callback_axis1(self, msg):
         if self.zeroangle1 is None:
             self.zeroangle1 = msg.pos_estimate
-        self.get_logger().info(f'Axis 1: pos_estimate={msg.pos_estimate}, vel_estimate={msg.vel_estimate}, torque_estimate={msg.torque_estimate}')
+        self.log_counter_axis1 += 1
+        if self.log_counter_axis1 % self.log_frequency == 0:
+            self.get_logger().info(f'Axis 1: pos_estimate={msg.pos_estimate}, vel_estimate={msg.vel_estimate}, torque_estimate={msg.torque_estimate}')
+            self.log_counter_axis1 = 0
 
     def control_message(self):
-        # For this specific script, we will initiate the leg in proned position and raise it to standard position
-        # (angles of 0 to angles of 45 deg)
-
         if self.zeroangle0 is not None and self.zeroangle1 is not None:
-            # Set motors to position control
             self.msg0.control_mode = 3
             self.msg1.control_mode = 3
-
-            # Set motor angles to 45 deg
             self.msg0.input_pos = 45 * (3.1415 / 180) - self.zeroangle0
             self.msg1.input_pos = 45 * (3.1415 / 180) - self.zeroangle1
-
-            # Publish control messages
             self.control0.publish(self.msg0)
             self.control1.publish(self.msg1)
 
