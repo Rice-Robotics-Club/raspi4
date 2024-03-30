d58fca6f7dc84068426bad0f68615a2c5366448e
winsonlin8 <112127031+winsonlin8@users.noreply.github.com>
2024-03-30 18:51:21 -0500

    added servos

--- ros2_ws/src/catbot/catbot/jump.py
+++ ros2_ws/src/catbot/catbot/jump.py
@@ -2,211 +2,123 @@ import rclpy
 from rclpy.node import Node
 from odrive_can.srv import AxisState
 from odrive_can.msg import ControlMessage, ControllerStatus
+from std_msgs.msg import Float64
+
+TORQUE_SPIKE_THRESHOLD = 0.05  # Need to find actual working threshold for torque spike detection
+MAX_TORQUE = -0.48
 
 class ControlNode(Node):
     def __init__(self):
-        super().__init__('simple_node')
+        super().__init__('control_node')
 
         self.phase = 'Proning'  # Initial phase
         self.poising_torque = 0.24  # [Nm]
-        self.poising_timer = None
-        self.pouncing_timer = None
         self.bracing_angle = 70 * (3.1415 / 180)  # [rad]
-        self.bracing_timer = None
         self.standard_angle = 45 * (3.1415 / 180)  # [rad]
 
         # Create a client for the set_axis_state service
-        self.state0 = self.create_client(AxisState, '/odrive_axis0/request_axis_state')
-        self.state1 = self.create_client(AxisState, '/odrive_axis1/request_axis_state')
-        while not self.state0.wait_for_service(timeout_sec=1.0) or not self.state0.wait_for_service(timeout_sec=1.0):
+        self.state_client = self.create_client(AxisState, '/odrive_axis0/request_axis_state')
+        while not self.state_client.wait_for_service(timeout_sec=1.0):
             self.get_logger().info('waiting for AxisState service...')
         self.axis_request = AxisState.Request()
 
-        # Set up subscribers
-        self.controller_status0 = self.create_subscription(
+        # Set up subscriber for the ODrive controller status
+        self.controller_status_subscriber = self.create_subscription(
             ControllerStatus,
             '/odrive_axis0/controller_status',
-            self.controller_status_callback_axis0,
-            10
-        )
-        self.controller_status1 = self.create_subscription(
-            ControllerStatus,
-            '/odrive_axis1/controller_status',
-            self.controller_status_callback_axis1,
+            self.controller_status_callback,
             10
         )
 
-        # Create ControlMessage objects for both motors
-        self.msg0 = ControlMessage()
-        self.msg0.control_mode = 2  # Set to pass-through mode initially
-        self.msg0.input_mode = 1
-        self.msg1 = ControlMessage()
-        self.msg1.control_mode = 2  # Set to pass-through mode initially
-        self.msg1.input_mode = 1
+        # Create a ControlMessage object for the motor
+        self.motor_msg = ControlMessage()
+        self.motor_msg.control_mode = 2  # Set to pass-through mode initially
+        self.motor_msg.input_mode = 1
 
-        # Reference angles for the motors will be updated in the callbacks
-        self.zeroangle0 = None
-        self.zeroangle1 = None
+        # Set up publisher to the ODrive
+        self.motor_control_publisher = self.create_publisher(ControlMessage, '/odrive_axis0/control_message', 10)
 
-        # Set up publishers to the ODrives
-        self.control0 = self.create_publisher(ControlMessage, 'control_message_0', 10)
-        self.control1 = self.create_publisher(ControlMessage, 'control_message_1', 10)
+        # Set up publisher for the servo
+        self.servo_publisher = self.create_publisher(Float64, 'servo_angle', 10)
 
-        # Logging frequency control
-        self.log_counter_axis0 = 0
-        self.log_counter_axis1 = 0
-        self.log_frequency = 25  # Adjust this value to change the logging frequency
+        # Reference angle for the motor will be updated in the callback
+        self.zero_angle = None
 
-    def controller_status_callback_axis0(self, msg):
-        if self.zeroangle0 is None:
-            self.zeroangle0 = msg.pos_estimate
-        self.log_counter_axis0 += 1
-        if self.log_counter_axis0 % self.log_frequency == 0:
-            self.get_logger().info(f'Axis 0: pos_estimate={msg.pos_estimate}, vel_estimate={msg.vel_estimate}, torque_estimate={msg.torque_estimate}')
-            self.log_counter_axis0 = 0
+    def controller_status_callback(self, msg):
+        if self.zero_angle is None:
+            self.zero_angle = msg.pos_estimate
 
-    def controller_status_callback_axis1(self, msg):
-        if self.zeroangle1 is None:
-            self.zeroangle1 = msg.pos_estimate
-        self.log_counter_axis1 += 1
-        if self.log_counter_axis1 % self.log_frequency == 0:
-            self.get_logger().info(f'Axis 1: pos_estimate={msg.pos_estimate}, vel_estimate={msg.vel_estimate}, torque_estimate={msg.torque_estimate}')
-            self.log_counter_axis1 = 0
-
-    def control_message(self):
         if self.phase == 'Proning':
-            if self.zeroangle0 is not None and self.zeroangle1 is not None:
-                # Set motors to position control
-                self.msg0.control_mode = 3
-                self.msg1.control_mode = 3
-                # Set both motors to 0 position (proned position)
-                self.msg0.input_pos = 0 - self.zeroangle0
-                self.msg1.input_pos = 0 - self.zeroangle1
-                # Publish control messages
-                self.control0.publish(self.msg0)
-                self.control1.publish(self.msg1)
-                # Update phase to Poising
-                self.phase = 'Poising'
-                self.get_logger().info('Transitioning to Poising phase')
+            # Set motor to position control
+            self.motor_msg.control_mode = 3
+            # Set motor to 0 position (proned position)
+            self.motor_msg.input_pos = 0 - self.zero_angle
+            # Publish control message for the motor
+            self.motor_control_publisher.publish(self.motor_msg)
+            # Move servo to connect the legs
+            self.set_servo_angle(180)
+            # Update phase to Poising
+            self.phase = 'Poising'
+            self.get_logger().info('Transitioning to Poising phase')
                 
         elif self.phase == 'Poising':
-            if not self.poising_timer:
-                # TODO: Swing out the servo (implementation depends on servo control)
-                # Set bottom motor to torque control
-                self.msg1.control_mode = 1
-                # Set bottom motor torque to poising_torque
-                self.msg1.input_torque = self.poising_torque
-                # Publish control message for bottom motor
-                self.control1.publish(self.msg1)
-                self.get_logger().info('Poising: Torque set for bottom motor')
-                # Set a timer to transition to the next phase after 0.5 seconds
-                self.poising_timer = self.create_timer(0.5, self.transition_to_pouncing)
-            else:
-                # The poising timer is already set, so just wait for it to expire
-                pass
-                
+            # Set motor to torque control
+            self.motor_msg.control_mode = 1
+            # Set motor torque to poising_torque
+            self.motor_msg.input_torque = self.poising_torque
+            # Publish control message for the motor
+            self.motor_control_publisher.publish(self.motor_msg)
+            self.get_logger().info('Poising: Torque set for the motor')
+            # Update phase to Pouncing
+            self.phase = 'Pouncing'
+            self.get_logger().info('Transitioning to Pouncing phase')
+
         elif self.phase == 'Pouncing':
-            if not self.pouncing_timer:
-                # Swing in the servo (implementation depends on servo control)
-                # Set top motor to position control
-                self.msg0.control_mode = 3
-                # Set bottom motor to torque control
-                self.msg1.control_mode = 1
-                # Set top leg angle to 70 degrees
-                self.msg0.input_pos = self.bracing_angle - self.zeroangle0
-                # Publish control messages
-                self.control0.publish(self.msg0)
-                self.control1.publish(self.msg1)
-                self.get_logger().info('Pouncing: Top motor set to 70 degrees, bottom motor set to max torque')
+            # Release the servo to allow the spring to actuate the bottom leg
+            self.set_servo_angle(90)
+            # Set motor to provide maximum torque
+            self.motor_msg.input_torque = MAX_TORQUE
+            # Publish control message for the motor
+            self.motor_control_publisher.publish(self.motor_msg)
+            self.get_logger().info('Pouncing: Motor set to max torque')
+            # Update phase to Bracing
+            self.phase = 'Bracing'
+            self.get_logger().info('Transitioning to Bracing phase')
 
-                # Set a timer to transition to the next phase after 0.125 seconds
-                self.pouncing_timer = self.create_timer(0.125, self.transition_to_bracing)
-            else:
-                # The pouncing timer is already set, so just wait for it to expire
-                pass
-                
         elif self.phase == 'Bracing':
-            if not self.bracing_timer:
-                # Set both motors to position control
-                self.msg0.control_mode = 3
-                self.msg1.control_mode = 3
-                # Set both motors to 70 degrees (bracing angle)
-                self.msg0.input_pos = self.bracing_angle - self.zeroangle0
-                self.msg1.input_pos = self.bracing_angle - self.zeroangle1
-                # Publish control messages
-                self.control0.publish(self.msg0)
-                self.control1.publish(self.msg1)
-                self.get_logger().info('Bracing: Both motors set to 70 degrees')
+            # Set motor to position control
+            self.motor_msg.control_mode = 3
+            # Set motor to bracing angle
+            self.motor_msg.input_pos = self.bracing_angle - self.zero_angle
+            # Publish control message for the motor
+            self.motor_control_publisher.publish(self.motor_msg)
+            self.get_logger().info('Bracing: Motor set to bracing angle')
+            # Update phase to Landing
+            self.phase = 'Landing'
+            self.get_logger().info('Transitioning to Landing phase')
 
-                # Set a timer to transition to the next phase after 0.125 seconds
-                self.bracing_timer = self.create_timer(0.125, self.transition_to_landing)
-            else:
-                # The bracing timer is already set, so just wait for it to expire
-                pass
-                
         elif self.phase == 'Landing':
-            # Set both motors to position control
-            self.msg0.control_mode = 3
-            self.msg1.control_mode = 3
-            # Set both motors to the standard angle
-            self.msg0.input_pos = self.standard_angle - self.zeroangle0
-            self.msg1.input_pos = self.standard_angle - self.zeroangle1
-            # Publish control messages
-            self.control0.publish(self.msg0)
-            self.control1.publish(self.msg1)
-            self.get_logger().info('Landing: Both motors set to standard angle')
-
+            # Set motor to position control
+            self.motor_msg.control_mode = 3
+            # Set motor to standard angle
+            self.motor_msg.input_pos = self.standard_angle - self.zero_angle
+            # Publish control message for the motor
+            self.motor_control_publisher.publish(self.motor_msg)
+            self.get_logger().info('Landing: Motor set to standard angle')
             # Reset phase to 'Proning' for the next jump
             self.phase = 'Proning'
             self.get_logger().info('Jump completed. Resetting to Proning phase for next jump')
 
-        elif self.phase == 'Landing':
-            # Set both motors to position control
-            self.msg0.control_mode = 3
-            self.msg1.control_mode = 3
-            # Set both motors to the zeroangle position
-            self.msg0.input_pos = 0 - self.zeroangle0
-            self.msg1.input_pos = 0 - self.zeroangle1
-            # Publish control messages
-            self.control0.publish(self.msg0)
-            self.control1.publish(self.msg1)
-            self.get_logger().info('Landing: Both motors set to zeroangle position')
-            self.get_logger().info('Jump completed.')
+    def set_servo_angle(self, angle):
+        angle_msg = Float64()
+        angle_msg.data = angle
+        self.servo_publisher.publish(angle_msg)
 
-    def transition_to_pouncing(self):
-        self.phase = 'Pouncing'
-        self.get_logger().info('Transitioning to Pouncing phase')
-        # Cancel the poising timer and reset it to None
-        self.poising_timer.cancel()
-        self.poising_timer = None
-    
-    def transition_to_bracing(self):
-        self.phase = 'Bracing'
-        self.get_logger().info('Transitioning to Bracing phase')
-        # Cancel the pouncing timer and reset it to None
-        self.pouncing_timer.cancel()
-        self.pouncing_timer = None
-
-    def transition_to_landing(self):
-        self.phase = 'Landing'
-        self.get_logger().info('Transitioning to Landing phase')
-        # Cancel the bracing timer and reset it to None
-        self.bracing_timer.cancel()
-        self.bracing_timer = None
-
-    
-    def set_axis_state(self, s):
-        self.axis_request.axis_requested_state = s
-        
-        # /odrive_axis0/
-        self.future = self.state0.call_async(self.axis_request)
-        rclpy.spin_until_future_complete(self, self.future)
-        self.get_logger().info(f'Result for odrive_axis0: {self.future.result().axis_state}')
-        
-        # /odrive_axis1/
-        self.future = self.state1.call_async(self.axis_request)
-        rclpy.spin_until_future_complete(self, self.future)
-        self.get_logger().info(f'Result for odrive_axis1: {self.future.result().axis_state}')
+    def set_axis_state(self, state):
+        self.axis_request.axis_requested_state = state
+        future = self.state_client.call_async(self.axis_request)
+        rclpy.spin_until_future_complete(self, future)
+        self.get_logger().info(f'Result for odrive_axis0: {future.result().axis_state}')
 
 def main(args=None):
     rclpy.init(args=args)
