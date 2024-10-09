9cd51d8bc52124b9211dcd1a2128f5075682dc3e
Justin Lopato <justin@lopato.org>
2024-10-08 19:39:22 -0500

    modified ik node for single leg

--- ros2_ws/src/servobot/servobot/ik_node.py
+++ ros2_ws/src/servobot/servobot/ik_node.py
@@ -34,16 +34,16 @@ class IKNode(Node):
             raise Exception("incorrect number of position arguments")
         
         fl_pos = tuple(msg.data[0:3])
-        fr_pos = tuple(msg.data[3:6])
-        bl_pos = tuple(msg.data[6:9])
-        br_pos = tuple(msg.data[9:])
+        # fr_pos = tuple(msg.data[3:6])
+        # bl_pos = tuple(msg.data[6:9])
+        # br_pos = tuple(msg.data[9:])
         
         fl = self.controller.solve_leg(fl_pos, leg=0)
-        fr = self.controller.solve_leg(fr_pos, leg=1)
-        bl = self.controller.solve_leg(bl_pos, leg=2)
-        br = self.controller.solve_leg(br_pos, leg=3)
+        # fr = self.controller.solve_leg(fr_pos, leg=1)
+        # bl = self.controller.solve_leg(bl_pos, leg=2)
+        # br = self.controller.solve_leg(br_pos, leg=3)
         
-        self.publish(fl, fr, bl, br)
+        self.publish(fl, (), (), ())
 
     def publish(
         self, fl: tuple[float], fr: tuple[float], bl: tuple[float], br: tuple[float]
