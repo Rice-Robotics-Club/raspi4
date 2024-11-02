9ae7ca8754439b0dc7a687a3338c451ff806d271
Justin Lopato <justin@lopato.org>
2024-11-02 11:51:43 -0500

    added odrive packages and testgui packages

--- ros2_ws/src/servobot/servobot/circle_test_node.py
+++ ros2_ws/src/servobot/servobot/circle_test_node.py
@@ -1,6 +1,7 @@
 import rclpy
 from std_msgs.msg import Float64MultiArray
 from rclpy.node import Node
+from rclpy.parameter import Parameter
 import math
 import typing
 
@@ -11,25 +12,42 @@ class CircleTestNode(Node):
 
         self.get_logger().info(f"initializing {self.get_name()}")
 
-        self.radius: float = self.declare_parameter("radius", 1.0).value
-        self.height: float = self.declare_parameter("height", 4.5).value
-        period: float = self.declare_parameter("period", 5.0).value
+        self.radius = self.declare_parameter("radius", 1.0)
+        self.height = self.declare_parameter("height", 4.5)
+        self.period = self.declare_parameter("period", 5.0)
+        self.interval = self.declare_parameter("interval", 0.01)
+        self.add_on_set_parameters_callback(self.set_parameters)
 
         self.leg_positions = self.create_publisher(
             Float64MultiArray, "/leg_positions", 10
         )
 
-        interval = 0.01
-        self.timer = self.create_timer(interval, self.timer_callback)
+        self.timer = self.create_timer(self.interval, self.timer_callback)
+        
+        self.set_parameters([
+            self.radius,
+            self.height,
+            self.period,
+            self.interval
+        ])
 
         self.msg = Float64MultiArray()
         self.msg.data = []
-        self.angle = 0.0
-        self.delta = (math.tau * interval) / period
+
+    def set_parameters(self, params: list[Parameter]):
+        params = {p.name: p for p in params}
+        
+        self.radius = params["radius"].value
+        self.height = params["height"].value
+        self.period = params["period"].value
+        self.interval = params["interval"].value
+        
+        self.delta = (math.tau * self.interval) / self.period
+        self.timer.timer_period_ns = self.interval * 1000000000.0
 
     def timer_callback(self) -> None:
         l = [
-        	0.7,
+            0.7,
             -self.height,
             self.radius * math.cos(self.angle),
         ] * 4
